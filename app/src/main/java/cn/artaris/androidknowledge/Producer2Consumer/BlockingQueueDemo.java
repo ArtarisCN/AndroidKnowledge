d5226b6a46b3e0df866f353afef40b2e3b6f8ff1
Rick Lee <artaris@sina.com>
2019-06-12 12:02:20 +0800

    Producer2Consumer

---
+++ app/src/main/java/cn/artaris/androidknowledge/Producer2Consumer/BlockingQueueDemo.java
@@ -0,0 +1,68 @@
+package cn.artaris.androidknowledge.Producer2Consumer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * cn.artaris.androidknowledge.Producer2Consumer
+ * AndroidKnowledge
+ * 2019.06.12  11:59
+ *
+ * @author : artairs
+ */
+public class BlockingQueueDemo {
+    private static Integer count = 0;
+    //创建一个阻塞队列
+    final BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(10);
+    public static void main(String[] args) {
+        BlockingQueueDemo test3 = new BlockingQueueDemo();
+        new Thread(test3.new Producer()).start();
+        new Thread(test3.new Consumer()).start();
+        new Thread(test3.new Producer()).start();
+        new Thread(test3.new Consumer()).start();
+        new Thread(test3.new Producer()).start();
+        new Thread(test3.new Consumer()).start();
+        new Thread(test3.new Producer()).start();
+        new Thread(test3.new Consumer()).start();
+    }
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    blockingQueue.put(1);
+                    count++;
+                    System.out.println(Thread.currentThread().getName()
+                            + "生产者生产，目前总共有" + count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    class Consumer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                try {
+                    blockingQueue.take();
+                    count--;
+                    System.out.println(Thread.currentThread().getName()
+                            + "消费者消费，目前总共有" + count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/cn/artaris/androidknowledge/Producer2Consumer/Wait_Notify.java
@@ -0,0 +1,74 @@
+package cn.artaris.androidknowledge.Producer2Consumer;
+
+/**
+ * cn.artaris.androidknowledge.Producer2Consumer
+ * AndroidKnowledge
+ * 2019.06.12  11:56
+ *
+ * @author : artairs
+ */
+public class Wait_Notify {
+    private static Integer count = 0;
+    private static final Integer FULL = 10;
+    private static final String LOCK = "lock";
+
+    public static void main(String[] args) {
+        Wait_Notify test1 = new Wait_Notify();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+        new Thread(test1.new Producer()).start();
+//        new Thread(test1.new Consumer()).start();
+        new Thread(test1.new Producer()).start();
+        new Thread(test1.new Consumer()).start();
+    }
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count.equals(FULL)) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    count++;
+                    System.out.println(Thread.currentThread().getName() + "生产者生产，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
+    class Consumer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == 0) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                            e.fillInStackTrace();
+                        }
+                    }
+                    count--;
+                    System.out.println(Thread.currentThread().getName() + "消费者消费，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
+}
